9d630e692f4cc3a30fb864491f45d01785c48ce3
thomas <thomas.faget@etu.enseeiht.fr>
2017-10-23 11:29:01 -0400

    Add leader

---
+++ Leader.cpp
@@ -0,0 +1,30 @@
+﻿#include "Leader.h"
+#include "SteeringBehaviors.h"
+
+Leader::Leader(
+    GameWorld* world, 
+    Vector2D position, 
+    double rotation, 
+    Vector2D velocity, 
+    double mass, 
+    double max_force,
+    double max_speed, 
+    double max_turn_rate, 
+    double scale): 
+Vehicle(
+    world,
+    position,
+    rotation,
+    velocity,
+    mass,
+    max_force,
+    max_speed,
+    max_turn_rate,
+    scale ) {
+    Leader::SetScale( Vector2D( 10, 10 ) );
+    Leader::Steering()->WanderOn();
+    Leader::SetMaxSpeed( 100 );
+    Leader::SetMaxForce( 10 );
+}
+
+Leader::~Leader() {}
---
+++ Leader.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Vehicle.h"
+
+class Leader : public Vehicle {
+public:
+    
+    Leader( GameWorld* world,
+        Vector2D position,
+        double rotation,
+        Vector2D velocity,
+        double mass,
+        double max_force,
+        double max_speed,
+        double max_turn_rate,
+        double scale);
+
+
+    ~Leader();
+};
